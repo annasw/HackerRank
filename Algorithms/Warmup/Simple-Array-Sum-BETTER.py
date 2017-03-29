6f08722e29e1ef6d5bc635353b63e85ea9ad5b84
Jesse White <jsperlingwhite@gmail.com>
2017-03-29 16:06:07 -0700

    Create Simple-Array-Sum-BETTER.py

---
+++ Algorithms/Warmup/Simple-Array-Sum-BETTER.py
@@ -0,0 +1,4 @@
+# Since I didn't get to actually write any code in the last Simple-Array-Sum.py,
+# here's a one-liner solution
+
+print sum([int(raw_input())*0]+[int(i) for i in raw_input().split(' ')])
