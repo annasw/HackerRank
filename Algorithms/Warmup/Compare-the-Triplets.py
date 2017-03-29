37981835edd3f143a7817ca3c8c9ac3725ddb54e
Jesse White <jsperlingwhite@gmail.com>
2017-03-29 16:09:08 -0700

    Create Compare-the-Triplets.py

---
+++ Algorithms/Warmup/Compare-the-Triplets.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+import sys
+
+
+a0,a1,a2 = raw_input().strip().split(' ')
+a0,a1,a2 = [int(a0),int(a1),int(a2)]
+b0,b1,b2 = raw_input().strip().split(' ')
+b0,b1,b2 = [int(b0),int(b1),int(b2)]
+
+print sum([a0>b0, a1>b1, a2>b2]), sum([b0>a0, b1>a1, b2>a2])
