7caaf774e0f5146296f1533df10cc79010cef8cd
Jesse White <jsperlingwhite@gmail.com>
2017-03-29 15:48:47 -0700

    Create Simple-Array-Sum.py

---
+++ Algorithms/Warmup/Simple-Array-Sum.py
@@ -0,0 +1,17 @@
+# this was literally all done for me
+# literally
+# the only thing I did
+# was press "Submit Code"
+# (and write these comments)
+
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
+sum = 0
+for x in arr: sum+=x
+print sum
