4945ff5edeac699288ab7c20d28d41c1cfa5749b
Jesse White <jsperlingwhite@gmail.com>
2017-04-07 21:58:38 -0700

    Create Day-15-Linked-List.py

---
+++ 30DoC/Day-15-Linked-List.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print current.data,
+            current = current.next	
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head==None: return Node(data)
+        h = head
+        while h.next!=None:
+            h = h.next
+        h.next = Node(data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head);
