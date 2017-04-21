35db0a7e91dac5dee995f3a5aa0fbedd4e716b60
Jesse White <jsperlingwhite@gmail.com>
2017-04-20 22:46:03 -0700

    Update Input-and-Output.cpp
    Replaced with more compact code.

--- CPP/Introduction/Input-and-Output.cpp
+++ CPP/Introduction/Input-and-Output.cpp
@@ -7,10 +7,7 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int a;
-    int b;
-    int c;
+    int a, b, c;
     cin >> a >> b >> c;
     cout << a+b+c;
     return 0;
