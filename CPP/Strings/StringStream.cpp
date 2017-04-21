d2a99019b25b09be2badb62f39f1c9138a01c16c
Jesse White <jsperlingwhite@gmail.com>
2017-04-21 01:51:26 -0700

    Create StringStream.cpp

---
+++ CPP/Strings/StringStream.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> nums;
+    int i;
+    char c;
+    while (ss) {
+        ss >> i >> c;
+        nums.push_back(i);
+    }
+    return nums;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
