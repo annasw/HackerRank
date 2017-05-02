023b2b6f22ba0e1a979fac45627d34b57fe77e44
Jesse White <jsperlingwhite@gmail.com>
2017-05-01 22:32:45 -0700

    Update Binary-Search-Tree-Identification-Better.py
    improved comments to explain why this implementation is better than the other one

--- CtCI/Binary-Search-Tree-Identification-Better.py
+++ CtCI/Binary-Search-Tree-Identification-Better.py
@@ -7,13 +7,14 @@ class node:
         self.left = None
         self.right = None
 """
+# importing inf lets us start with lo and hi outside any possible finite value of the tree,
+# rather than having to start with fixed limits or figure them out beforehand.
 from math import inf
 
-# we use optional arguments to avoid the need to write a second, recursive method
+# we use optional arguments to avoid the need to write a second method
 def check_binary_search_tree_(root, lo=-inf, hi=inf):
     # and we check None here to avoid the need to check awkwardly on the initial call and each recursive call.
     if root==None: return True
     if root.data >= hi or root.data <= lo: return False
 
     return check_binary_search_tree_(root.left, lo, root.data) and check_binary_search_tree_(root.right, root.data, hi)
-    
